8bb41ead491d3857788df1eb77af67acbf719bea
daixiquan <daixiquan@gotokeep.com>
2018-11-21 00:01:26 +0800

    fit params

--- decision_tree/main.py
+++ decision_tree/main.py
@@ -1,56 +1,65 @@
-import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import ensemble
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import classification_report
 
 import pandas as pd
 
 
 def load_data():
-    # titanic = pd.read_csv('/Users/daixiquan/Documents/workout/decision_tree/resource/titanic.txt')
     keeplandData = pd.read_csv('/Users/daixiquan/Documents/workout/decision_tree/resource/keepland_user_profile.csv')
     keeplandData.head()
     # keeplandData.info()
 
-    # X = titanic[['bookcount', 'trainingcount', 'age', 'gender', 'goal', 'trainingduration', 'trainingcalorie',
-    #              'trainingtimes', 'trainingdays', 'runningtimes', 'cyclingtimes', 'hikingtimes', 'allduration',
-    #              'allcalorie', 'alltimes', 'alldays', 'traininggoal', 'difficulty', 'maxcomboday', 'kg', 'bmi',
-    #              'joinedplans', 'plans']]
     X = keeplandData[
         ['bookcount', 'trainingcount', 'age', 'gender', 'goal', 'trainingtimes', 'trainingdays', 'allduration',
-         'allcalorie', 'alltimes', 'alldays', 'traininggoal', 'difficulty', 'maxcomboday', 'kg', 'bmi']]
+         'allcalorie', 'maxcomboday', 'kg', 'bmi']]
+
     y = keeplandData['bought_package']
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.25, random_state=33)
     vec = DictVectorizer(sparse=False)
     X_train = vec.fit_transform(X_train.to_dict(orient='record'))
     X_test = vec.fit_transform(X_test.to_dict(orient='record'))
 
-    return X_train, X_test, Y_train, Y_test
+    return X_train, X_test, Y_train, Y_test, vec.feature_names_
 
 
-x_train, x_test, y_train, y_test = load_data()
+x_train, x_test, y_train, y_test, feature_names = load_data()
+
+
+def searchBestParams(clf):
+    # 首先对n_estimators进行网格搜索
+    param_test1 = {'n_estimators': range(10, 71, 10)}
+    gsearch1 = GridSearchCV(clf, param_grid=param_test1, scoring='roc_auc', cv=5)
+    gsearch1.fit(x_train, y_train)
+    print(gsearch1.best_estimator_, gsearch1.best_score_)
 
 
 def try_different_method(clf, title):
     clf.fit(x_train, y_train)
-    score = clf.score(x_test, y_test)
     result = clf.predict(x_test)
-    plt.figure()
-    plt.plot(np.arange(len(result)), y_test, 'go-', label='true value')
-    plt.plot(np.arange(len(result)), result, 'ro-', label='predict value')
-    plt.title('%s score: %f' % (title, score))
-    plt.legend()
-    plt.show()
+    score = clf.score(x_test, y_test)
+    print(title)
+    print(score)
+    print(classification_report(y_test, result, target_names=["not buy", "buy"]))
+    print("\n")
 
 
 if __name__ == "__main__":
-    # dt = DecisionTreeClassifier(criterion="entropy",max_depth=5,)
-    # try_different_method(dt, "decision tree")
+    dt = DecisionTreeClassifier(criterion="entropy", max_depth=5)
+    try_different_method(dt, "DecisionTree")
 
-    rf = ensemble.RandomForestRegressor(n_estimators=50,max_depth=5)  # 这里使用20个决策树
-    try_different_method(rf, "random forest")
-    #
-    # ada = ensemble.AdaBoostRegressor(n_estimators=10)
-    # try_different_method(ada, "ada boost")
+    rf = ensemble.RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gini',
+                                         max_depth=8, max_features='sqrt', max_leaf_nodes=None,
+                                         min_impurity_decrease=0.0, min_impurity_split=None,
+                                         min_samples_leaf=20, min_samples_split=100,
+                                         min_weight_fraction_leaf=0.0, n_estimators=50, n_jobs=None,
+                                         oob_score=False, random_state=10, verbose=0, warm_start=False)
+    try_different_method(rf, "RandomForest")
+    # searchBestParams(rf)
+
+    ada = ensemble.AdaBoostClassifier(algorithm='SAMME.R', base_estimator=None,
+                                      learning_rate=1.0, n_estimators=40, random_state=None)
+    try_different_method(ada, "AdaBoost")
+    # searchBestParams(ada)
