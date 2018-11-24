a4f09a7a4f006d9f9c19d7535fef0a83b7641b8b
daixiquan <daixiquan@gotokeep.com>
2018-11-24 18:14:16 +0800

    add

---
+++ sentiment_analysis/pre_process.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+from nltk.tokenize import WordPunctTokenizer
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
+from replacers import RegexReplacer
