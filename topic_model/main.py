6cac21882862ea6a221eecb5d36edd494d3a669d
shuanzi <daixiquan@gmail.com>
2018-12-28 22:36:40 +0800

    update

--- topic_model/main.py
+++ topic_model/main.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer as TFIV
 from sklearn.linear_model import LogisticRegression as LR
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.ensemble import BaggingClassifier
 
 
 def print_top_words(model, feature_names, n_top_words):
@@ -13,8 +14,8 @@ def print_top_words(model, feature_names, n_top_words):
         print "Topic #%d:" % topic_idx
         print " ".join([feature_names[i]
                         for i in topic.argsort()[:-n_top_words - 1:-1]])
-    print
     print model.components_
+
 
 class Preprocessing():
     def __init__(self, train_data_path, test_data_path, Unlabeled=None):
@@ -44,15 +45,37 @@ class Preprocessing():
         X_all = self.traindata + self.testdata
         lentrain = len(self.traindata)
 
-        tf = vectorizer.fit_transform(X_all)
+        vectorizer.fit(X_all)
+        X_all = vectorizer.transform(X_all)
 
-        n_topics = 30
-        lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=50, learning_method='batch')
-        lda.fit(tf)
+        self.X = X_all[:lentrain]
+        self.X_test = X_all[lentrain:]
 
-        n_top_words=20
-        tf_feature_names = vectorizer.get_feature_names()
-        print_top_words(lda, tf_feature_names, n_top_words)
+        print("vectorization data size: ", self.X.shape)
+        return self.X, self.y_train, self.X_test, self.y_test
+
+    # def vec(self):
+    #     vectorizer = CountVectorizer(analyzer="word", tokenizer=None, preprocessor=None, stop_words=None)
+    #
+    #     X_all = self.traindata + self.testdata
+    #     lentrain = len(self.traindata)
+    #
+    #     tf = vectorizer.fit_transform("../data/neg")
+    #
+    #     lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=50, learning_method='batch')
+    #     clf = BaggingClassifier(base_estimator=lda)
+    #
+    #
+    #     clf.fit(self.X_train, self.Y_Train)
+    #
+    #     # n_topics = 30
+    #     # lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=50, learning_method='batch')
+    #     # lda.fit(tf)
+    #     #
+    #     # n_top_words=20
+    #     # tf_feature_names = vectorizer.get_feature_names()
+    #     # print_top_words(lda, tf_feature_names, n_top_words)
+
 
 class classify():
     def __init__(self, X_train, Y_Train, X_Test, Y_Test):
@@ -80,11 +103,19 @@ class classify():
         print("using mnb, score %s" % score)
         print(classification_report(self.Y_Test, y_predict))
 
+    def bg(self):
+        n_topics = 18
+        lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=50, learning_method='batch')
+        clf = BaggingClassifier(base_estimator=lda)
+        clf.fit(X_train, y_train)
+        y_predict = clf.predict(self.X_Test)
+        print(classification_report(self.Y_Test, y_predict))
+
 
 if __name__ == "__main__":
-    process = Preprocessing("../data/train.csv", "../data/test.csv")
+    process = Preprocessing("../resource/train.csv", "../resource/test.csv")
     process.build_data()
     X_train, y_train, X_test, Y_test = process.vec()
-    # clf = classify(X_train, y_train, X_test, Y_test)
-    # clf.mnb()
+    clf = classify(X_train, y_train, X_test, Y_test)
+    clf.bg()
     # clf.LR()
